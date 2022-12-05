d0289177cd40b37898ddfb9bcaae051997873583
apolinario <joaopaulo.passos+multimodal@gmail.com>
2022-12-05 17:56:08 +0100

    Try to bring back the queue UI

--- modules/ui.py
+++ modules/ui.py
@@ -698,7 +698,7 @@ def create_ui():
                     generation_info,
                     html_info
                 ],
-                show_progress=False,
+                show_progress=True,
             )
 
             txt2img_prompt.submit(**txt2img_args)
@@ -931,7 +931,7 @@ def create_ui():
                     generation_info,
                     html_info
                 ],
-                show_progress=False,
+                show_progress=True,
             )
 
             img2img_prompt.submit(**img2img_args)
@@ -1581,8 +1581,8 @@ def create_ui():
         with open(os.path.join(script_path, "user.css"), "r", encoding="utf8") as file:
             css += file.read() + "\n"
 
-    if not cmd_opts.no_progressbar_hiding:
-        css += css_hide_progressbar
+    #if not cmd_opts.no_progressbar_hiding:
+        #css += css_hide_progressbar
 
     interfaces += script_callbacks.ui_tabs_callback()
     interfaces += [(settings_interface, "Settings", "settings")]
@@ -1596,7 +1596,7 @@ def create_ui():
                 gr.HTML(f'''
                 <div class="gr-prose" style="max-width: 80%">
                     <h2>Simplified Automatic1111 Stable Diffusion WebUI on Hugging Face Spaces 🎨</h2>
-                    <p>You are enjoying a simplified version of the Automatic1111, you can duplicate this Space to skip the queue and run the full version.&nbsp;&nbsp;<a class="duplicate-button" style="display:inline-block" target="_blank" href="https://huggingface.co/spaces/{os.environ['SPACE_ID']}?duplicate=true"><img src="https://img.shields.io/badge/-Duplicate%20Space-blue?labelColor=white&style=flat&logo=data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAP5JREFUOE+lk7FqAkEURY+ltunEgFXS2sZGIbXfEPdLlnxJyDdYB62sbbUKpLbVNhyYFzbrrA74YJlh9r079973psed0cvUD4A+4HoCjsA85X0Dfn/RBLBgBDxnQPfAEJgBY+A9gALA4tcbamSzS4xq4FOQAJgCDwV2CPKV8tZAJcAjMMkUe1vX+U+SMhfAJEHasQIWmXNN3abzDwHUrgcRGmYcgKe0bxrblHEB4E/pndMazNpSZGcsZdBlYJcEL9Afo75molJyM2FxmPgmgPqlWNLGfwZGG6UiyEvLzHYDmoPkDDiNm9JR9uboiONcBXrpY1qmgs21x1QwyZcpvxt9NS09PlsPAAAAAElFTkSuQmCC&logoWidth=14" alt="Duplicate Space"></a></p> 
+                    <p>You are enjoying a simplified shared version of the Automatic1111, you can duplicate this Space to skip the queue and run the full version.&nbsp;&nbsp;<a class="duplicate-button" style="display:inline-block" target="_blank" href="https://huggingface.co/spaces/{os.environ['SPACE_ID']}?duplicate=true"><img style="border: 0" src="https://img.shields.io/badge/-Duplicate%20Space-blue?labelColor=white&style=flat&logo=data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAP5JREFUOE+lk7FqAkEURY+ltunEgFXS2sZGIbXfEPdLlnxJyDdYB62sbbUKpLbVNhyYFzbrrA74YJlh9r079973psed0cvUD4A+4HoCjsA85X0Dfn/RBLBgBDxnQPfAEJgBY+A9gALA4tcbamSzS4xq4FOQAJgCDwV2CPKV8tZAJcAjMMkUe1vX+U+SMhfAJEHasQIWmXNN3abzDwHUrgcRGmYcgKe0bxrblHEB4E/pndMazNpSZGcsZdBlYJcEL9Afo75molJyM2FxmPgmgPqlWNLGfwZGG6UiyEvLzHYDmoPkDDiNm9JR9uboiONcBXrpY1qmgs21x1QwyZcpvxt9NS09PlsPAAAAAElFTkSuQmCC&logoWidth=14" alt="Duplicate Space"></a></p> 
                 </div>
                 ''')
             elif(is_spaces):
@@ -1604,14 +1604,14 @@ def create_ui():
                 if(not torch.cuda.is_available()):
                     gr.HTML(f'''
                     <div class="gr-prose" style="max-width: 80%">
-                        <h2>Full Automatic1111 Stable Diffusion WebUI on Hugging Face Spaces 🎨</h2>
+                        <h2>Private Automatic1111 Stable Diffusion WebUI on Hugging Face Spaces 🎨</h2>
                         <p>For now it is running on CPU 🥶, which may be slow, upgrade for a GPU from US$0,60/h <a href="https://huggingface.co/spaces/{os.environ['SPACE_ID']}/settings" style="text-decoration: underline" target="_blank">in the Settings tab</a></p> 
                     </div>
                 ''')
                 else:
                     gr.HTML(f'''
                     <div class="gr-prose" style="max-width: 80%">
-                        <h2>Full Automatic1111 Stable Diffusion WebUI on Hugging Face Spaces 🎨</h2>
+                        <h2>Private Automatic1111 Stable Diffusion WebUI on Hugging Face Spaces 🎨</h2>
                         <p>It is running on a GPU 🔥, you can <a href="https://huggingface.co/spaces/{os.environ['SPACE_ID']}/settings" style="text-decoration: underline" target="_blank">don't forget to remove the GPU attribution</a> once your are done playing with it</p> 
                     </div>
                 ''')
